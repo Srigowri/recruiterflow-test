ba89afa88dc80d13eb49ee8c12308b8525d6d6ea
Srigowri <srigowri.venkatesha@gmail.com>
2022-07-27 22:04:26 +0530

    adding to app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+export default function App(){
+  /*
+    meals: stores the list of dishes fetched from API
+    click: keep track of add button event, to trigger async fetch
 
-function App() {
+  */
+  const [meals, setMeals] = React.useState([]);
+  const [click, setClick] = React.useState(false);
+  const mealCount = 10;  
+
+  React.useEffect(() => {
+    /*
+      Fetching meals first time on reload
+    */
+    async function getMeals() {
+      for (let i = 0; i < mealCount; i++) {
+        let meal = await getRandomMeal();
+        setMeals((old) => [...old, meal]);
+      }
+    }
+    if(!meals){
+      //if none of the meals are fetched before
+      getMeals();
+    }      
+  }, []);
+
+  // using local storage for caching
+  React.useEffect(() => {
+    localStorage.setItem("meals", JSON.stringify(meals));
+  }, [meals]);
+
+  //async fetch for a new meal based on click event
+  React.useEffect(() => {
+    async function addNewMeal() {
+      let meal = await getRandomMeal();
+      setMeals((old) => [meal, ...old]);      
+    }
+    addNewMeal();
+  }, [click]);
+
+  
+  function windowScroll(){
+    // the newly added dish is the first dish, so moving to the top of the page
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }
+  //handler when add meal button is clicked
+  function handleAddMeal() {
+    setClick(!click);
+    windowScroll();
+  }
+  //handler when delete meal button is clicked
+  function handleDeleteMeal(mealId) {    
+    setMeals(meals.filter((meal) => meal.idMeal !== mealId));
+  }
+  //the app controls the props sent to child -> Homepage
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Homepage
+        meals={meals}
+        handleAddMeal={handleAddMeal}
+        handleDeleteMeal={handleDeleteMeal}
+      />
+    </main>
   );
-}
+};
 
-export default App;
